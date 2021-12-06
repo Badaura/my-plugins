09bd9a06829caf06298c60104f251a3b6682e0bf
Badaura <93402900+Badaura@users.noreply.github.com>
2021-12-06 08:02:17 -0800

    Add files via upload

---
+++ prision.lua
@@ -0,0 +1,18 @@
+
+local Plugin = {
+    ["PluginName"] = "infinite prision destruction "..ver,
+    ["PluginDescription"] = "this plugin was maded by x1qz#1515, enjoy;)",
+    ["Commands"] = { 
+
+		["idk"] = {
+			["ListName"] = "idk2",
+			["Description"] = "idk2",
+			["Aliases"] = {"idka"},
+			["Function"] = function(args, speaker)
+                print("HOLAAAAAA AJDADSJKDKJ estoi probando perdon jajasjskkj")
+			end
+		},
+    }
+}
+
+return Plugin
