80302efb85c11d2ceb111a47a01fcbfa6bf162f2
Badaura <93402900+Badaura@users.noreply.github.com>
2021-12-11 19:00:44 -0800

    Update prision.lua

--- prision.lua
+++ prision.lua
@@ -1,16 +1,30 @@
-local Plugin = {
-    ["PluginName"] = "infinite prision destruction "..ver,
-    ["PluginDescription"] = "this plugin was maded by x1qz#1515, enjoy;)",
-    ["Commands"] = { 
+--[[
+this is not mine, i just upload in my github profile bc pastebin dont work in my country so sorry about coping and u know..
 
-	["idk"] = {
-		["ListName"] = "idk2",
-		["Description"] = "idk2",
-		["Aliases"] = {"idka"},
-		["Function"] = function(args, speaker)
-             	   print("HOLAAAAAA AJDADSJKDKJ estoi probando perdon jajasjskkj")
-		end
-	},
-    }
-}
-return Plugin
+loadstring()
+
+Join Infinite Yield group to download more plugins, chat about scripting, script share and more!, also so much documentation about plugis u know, cool things
+https://discord.gg/m46AjThpDR
+--]]
+
+
+local PluginAPI = {}
+function PluginAPI:CreatePlugin(name, description) 
+	local Functions = {}
+	local Plugin = {
+		["PluginName"] = name,
+		["PluginDescription"] = description,
+		["Commands"] = {}
+	}
+	function Functions:AddCommand(Name, ListName, Description, Aliases, Callback) 
+		Plugin["Commands"][Name] = {
+			["ListName"] = ListName,
+			["Description"] = Description,
+			["Aliases"] = Aliases,
+			["Function"] = Callback
+		}
+	end
+	return {PluginTable = Plugin, Functions = Functions}
+end
+
+return PluginAPI
